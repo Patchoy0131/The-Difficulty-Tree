544e65a1fb05c57f022bd34849150c57d1bffd13
Patchoy0131 <154054422+Patchoy0131@users.noreply.github.com>
2024-08-29 19:39:45 +0800

    V0.1
    Eh

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Difficulty Tree",
+	id: "mermodyaination",
+	author: "Patchoy0131",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Changed Stuff",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
